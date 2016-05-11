7227e016170f4fdc2db32a83b46cc6226d0f5ef8
Daniil Novoselov <Dan2803@yandex.ru>
2016-05-11 20:57:09 +0300

    init commit

---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Daniil Novoselov on 11.05.16.
+//  Copyright © 2016 Daniil Novoselov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by Daniil Novoselov on 11.05.16.
+//  Copyright © 2016 Daniil Novoselov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    // Бла бла бла
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
